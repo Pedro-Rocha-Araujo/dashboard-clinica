80ac1c1875578a7e2734af6bd4ceecafdbd10721
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-07-08 13:57:09 -0300

    Função de listar os pacientes está funcionando e conversando com o frontend do projeto

--- frontend/src/app/gerenciar-profissionais/page.tsx
+++ frontend/src/app/gerenciar-profissionais/page.tsx
@@ -37,7 +37,7 @@ export default function GerenciarProfissionais() {
   useEffect(()=>{
     getProfissionaisInativos()
     getProfissionaisAtivos()
-  })
+  }, [])
   
   return (
     <section className="gerenciar-profissionais">
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,42 @@
+'use client'
+
+interface Paciente {
+  _id: string,
+  nome: string,
+  createdAt: string,
+  updatedAt: string
+}
+
+interface SenhaAguardando {
+  _id: string,
+  numero: number,
+  paciente: Paciente,
+  status: "Aguardando" | "NaFila" | "Finalizado" | "Cancelado",
+  createdAt: string,
+  updatedAt: string
+}
+
+import { useState, useEffect } from "react"
+import axios from "axios"
 import Botoes from "../components/botoes-home/page"
 import "./home.css"
 
 export default function Home() {
+  const [senhas, setSenhas] = useState<SenhaAguardando[]>([])
+
+  async function getSenhas() {
+    try {
+      const response = await axios.get("http://localhost:4000/senha")
+      setSenhas(response.data)
+    } catch(erro) {
+      console.log(erro)
+    }
+  }
+
+  useEffect(()=>{
+    getSenhas()
+  }, [])
+
   return (
     <section className="home">
       <h2> <i className="fa-solid fa-house"></i> Página home</h2>
@@ -17,15 +52,26 @@ export default function Home() {
         </thead>
 
         <tbody>
-
-          <tr>
-            <td>1</td>
-            <td>João Souza Ramos</td>
-            <td>Aguardando</td>
-            <td>
-              <Botoes />
-            </td>
-          </tr>
+          { senhas.length > 0 ? (
+            senhas.map((senha)=>{
+              return (
+                  <tr key={senha._id}>
+                    <td>{senha.numero}</td>
+                    <td>{senha.paciente.nome}</td>
+                    <td>{senha.status}</td>
+                    <td>
+                      <Botoes />
+                    </td>
+                  </tr>
+              )
+            })
+          ): (
+            <tr>
+              <td colSpan={100}>
+                <p>Nenhuma senha até o momento.</p>
+              </td>
+            </tr>
+          ) }
         </tbody>
 
       </table>
