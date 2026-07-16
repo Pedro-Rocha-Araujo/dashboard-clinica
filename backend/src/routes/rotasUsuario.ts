b38d36d5878b00ac3709e3fd4018a6c7515ff4e7
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-07-16 13:56:36 -0300

    Função de logar usuario criada e adicionando o token na localstorage

--- backend/src/routes/rotasUsuario.ts
+++ backend/src/routes/rotasUsuario.ts
@@ -1,10 +1,12 @@
 import { Router } from "express"
 import {
   cadastrarRecepcionista,
+  logarUsuario
 } from "../controllers/controllersUsuario.js"
 
 const rotasUsuario = Router()
 
 rotasUsuario.post("/usuario", cadastrarRecepcionista)
+rotasUsuario.post("/usuario/login", logarUsuario)
 
 export default rotasUsuario
--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -1,9 +1,44 @@
 'use client'
 
 import "../usuario.css"
+import { useRef } from "react"
+import { useRouter } from "next/navigation"
 import Link from "next/link"
+import axios from "axios"
+import { toast } from "react-toastify"
 
 export default function Login() {
+  const router = useRouter()
+
+  const usuarioRef = useRef<HTMLInputElement | null>(null)
+  const senhaRef = useRef<HTMLInputElement | null>(null)
+
+  async function logarUsuario(e:React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    try {
+      const usuario = usuarioRef.current?.value.trim()
+      const senha = senhaRef.current?.value.trim()
+      if(!usuario || !senha) {
+        return toast.error("Preencha todos os campos.")
+      }
+      const response = await axios.post("http://localhost:4000/usuario/login", {
+        usuario: usuario,
+        senha: senha
+      })
+
+      const token = response.data.token
+      localStorage.setItem("token", token)
+      router.replace("/")
+    } catch(erro) {
+      if(axios.isAxiosError(erro)) {
+        return toast.error(erro.response?.data.Erro)
+      } else {
+        console.log(erro)
+        toast.error("Erro ao logar o usuario.")
+      }
+    }
+  }
+
   return (
     <section className="usuario">
       <div className="container">
@@ -12,9 +47,19 @@ export default function Login() {
           <h1>Login</h1>
         </div>
 
-        <form className="main">
-          <input type="text" placeholder="Digite um nome de Usuario" required />
-          <input type="password" placeholder="Disite sua senha de acesso" required />
+        <form className="main" onSubmit={logarUsuario} >
+          <input 
+            type="text" 
+            placeholder="Digite um nome de Usuario" 
+            ref={usuarioRef}
+            required 
+          />
+          <input 
+            type="password" 
+            placeholder="Disite sua senha de acesso" 
+            ref={senhaRef}
+            required 
+          />
           <button>Fazer Login</button>
         </form>
 
