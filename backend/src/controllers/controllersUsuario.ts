4af0ccd1a31e1a90d79ef77ea43693f957e3f95a
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-07-16 08:41:26 -0300

    Função de logar o usuário e retornando o token jwt criada no backend

--- backend/src/controllers/controllersUsuario.ts
+++ backend/src/controllers/controllersUsuario.ts
@@ -1,6 +1,8 @@
 import type { Request, Response } from "express"
 import UsuarioModel from "../models/Usuario.js"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import "dotenv/config"
 
 const salt = 10
 
@@ -32,6 +34,50 @@ export async function cadastrarRecepcionista(
   } catch(erro) {
     return response.status(500).json({
       Mensagem: "Erro ao cadastrar o Recepcionista",
+      erro: erro
+    })
+  }
+}
+
+export async function logarUsuario(
+  request:Request<{}, {}, UsuarioBody>, response:Response
+):Promise<Response> {
+  try {
+    const { usuario, senha } = request.body
+    if(!usuario || !senha) {
+      return response.status(400).json({ Erro: "Todos os campos são obrigatórios." })
+    }
+
+    const consulta = await UsuarioModel.findOne({ usuario: usuario.trim() })
+    if(!consulta) {
+      return response.status(401).json({ Erro: "Usuário e (ou) senha inválido(s)." })
+    }
+    
+    const comparacaoSenha = await bcrypt.compare(senha, consulta.senha)
+    if(!comparacaoSenha) {
+      return response.status(401).json({ Erro: "Usuário e (ou) senha inválido(s)." })
+    }
+
+    const senhaToken = process.env.SENHA_TOKEN
+    if(!senhaToken) {
+      return response.status(500).json({ Erro: "A senha do JWT não foi passada" })
+    }
+
+    const token = jwt.sign(
+      { 
+        id: consulta._id, 
+        usuario: consulta.usuario, 
+        tipo: consulta.tipo 
+      },
+      senhaToken,
+      { 
+        expiresIn: "1h" 
+      }
+    )
+    return response.status(200).json({token})
+  } catch(erro) {
+    return response.status(500).json({
+      Mensagem: "Erro ao logar o usuario",
       erro: erro
     })
   }
